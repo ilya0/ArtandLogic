03099b1a08dee27a643f192684a1bd340a0cf4b3
ilya0 <iosovets@gmail.com>
2017-10-03 20:21:03 -0700

    convert and decode works

--- artandlogic.js
+++ artandlogic.js
@@ -3,8 +3,8 @@
 function convertint(intermediate){
 
   //add 8192 to raw value
-  var intermediate = intermediate+8192;
-   console.log("intermediate decimal is "+intermediate);
+    var intermediate = intermediate+8192;
+    console.log("intermediate decimal is "+intermediate);
 
   // //intermediate decimal
   // var hexString = intermediate.toString(16);
@@ -18,15 +18,15 @@ function convertint(intermediate){
 //  console.log("upper byte " +upperbyte);
 
   //lower byte
- var intermediate_21 = intermediate;
- var intermediate_2 = (intermediate_21 &= 0x7F);
+  var intermediate_21 = intermediate;
+  var intermediate_2 = (intermediate_21 &= 0x7F);
 //  var lowerbyte = intermediate_2.toString(16);
 //  console.log("lower byte " +lowerbyte);
 
-   var intermediate_3 = intermediate_2 + ((intermediate &= 0xFF80)<<1);
- var encodedword = intermediate_3.toString(16);
+  var intermediate_3 = intermediate_2 + ((intermediate &= 0xFF80)<<1);
+  var encodedword = intermediate_3.toString(16);
 
-  if(encodedword == 0){encodedword = "0000";}
+//  if(encodedword == 0){encodedword = "0000";}
   console.log("encoded word " +encodedword);
 
 }
@@ -37,7 +37,7 @@ function convertint(intermediate){
 // convertint(-4096);
 // convertint(2048);
 // convertint(6111);
- convertint(2628);
+//convertint(2628);
 
 
 
@@ -46,37 +46,27 @@ function convertint(intermediate){
 
 //converts text to integer
 
-function convertbytes(hi,lo){
-  yourNumber = parseInt(hexString, 16);
-  console.log(text);
+function convertbytes(encodedinital){
+
+  var intermediate = parseInt(encodedinital, 16);
+  console.log(intermediate);
+
+  var intermediate_2 = intermediate;
+  var intermediate_3 = intermediate;
+
+  var intermediate_1 =  ((intermediate_2 &= 0x7F00) >>> 1) + (intermediate_3 &= 0x7F);
+  intermediate_1 = intermediate_1 -8192;
+
+//  var intermediatehex = intermediate_1.toString(16);
+  console.log("encoded word " +intermediate_1);
 }
 
 
 
+convertbytes("4000");
+convertbytes("0000");
+convertbytes("7f7f");
+convertbytes("5000");
+convertbytes("0A05");
+convertbytes("5500");
 
-
-
-
-// convertbytes(40,00);
-// convertbytes(00,00);
-// convertbytes(7f,7f);
-// convertbytes(50,00);
-// convertbytes(0A,05);
-// convertbytes(55,00);
-
-
-// range will be from  14-bit range [-8192..+8191]
-// return a 4 character
-
-
-/*
-The encoding process is as follows:
-1. Add 8192 to the raw value, so its range is translated to [0..16383]
-2. Pack that value into two bytes such that the most significant bit of each is cleared
-Unencoded intermediate value (as a 16-bit integer):
-00HHHHHH HLLLLLLL
-Encoded value:
-0HHHHHHH 0LLLLLLL
-3. Format the two bytes as a single 4-character hexadecimal string and return it.
-
-*/
