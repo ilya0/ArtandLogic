fe1f9885e5d0e3d8e79104fcf24569999c2daa24
ilya0 <iosovets@gmail.com>
2017-10-03 19:43:27 -0700

    convert works

--- artandlogic.js
+++ artandlogic.js
@@ -6,36 +6,55 @@ function convertint(intermediate){
   var intermediate = intermediate+8192;
    console.log("intermediate decimal is "+intermediate);
 
-  //intermediate decimal
-  var hexString = intermediate.toString(16);
-  console.log("intermediate hex " + hexString);
+  // //intermediate decimal
+  // var hexString = intermediate.toString(16);
+  // console.log("intermediate hex " + hexString);
+
+//  var intermediate_1 = intermediate >>> 7;
+//  console.log("intermediate_1 upper decimal is "+intermediate_1);
+
+//  //encoded upper byte
+// var upperbyte = intermediate_1.toString(16);
+//  console.log("upper byte " +upperbyte);
+
+  //lower byte
+ var intermediate_21 = intermediate;
+ var intermediate_2 = (intermediate_21 &= 0x7F);
+  var lowerbyte = intermediate_2.toString(16);
+  console.log("lower byte " +lowerbyte);
+
+   var intermediate_3 = intermediate_2 + ((intermediate &= 0xFF80)<<1);
+ var encodedword = intermediate_3.toString(16);
+
+  if(encodedword == 0){encodedword = "0000";}
+  console.log("encoded word " +encodedword);
+
+}
+
+// convertint(0);
+//convertint(-8192);
+// convertint(8191);
+// convertint(-4096);
+// convertint(2048);
+// convertint(6111);
+// convertint(2628);
 
 
-  var intermediate_1 = (intermediate / 128);
-  console.log("intermediate decimal is "+intermediate);
-
- // encoded upper byte
- var upperbyte = intermediate_1.toString(16);
-  console.log("upper byte " +upperbyte);
 
 
 
+
+//converts text to integer
+
+function convertbytes(hi,lo){
+  yourNumber = parseInt(hexString, 16);
+  console.log(text);
 }
 
 
 
 
-// //converts text to integer
-// function convertbytes(hi,lo){
-//   yourNumber = parseInt(hexString, 16);
-//   console.log(text);
-// }
 
-convertint(0);
-
-// convertint(-8192);
-// convertint(8191);
-// convertint(-4096);
 
 
 // convertbytes(40,00);
